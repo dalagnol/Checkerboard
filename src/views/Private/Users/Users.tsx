5a26ef01d0faae872b3b3f999f450cf4de9af676
Júlia Klebanowycz <juliadak@icloud.com>
2020-07-05 13:11:21 -0300

    users list

--- src/views/Private/Users/Users.tsx
+++ src/views/Private/Users/Users.tsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import { useTheme } from "theme";
 import { themejson } from "./json";
+import { UserContext } from "controllers";
 
-import { Container, Main, Title } from "./styles";
+import {
+  Container,
+  Main,
+  Title,
+  Users as Element,
+  UserTile,
+  Header,
+  Subheader,
+  Delete
+} from "./styles";
 
 export function Users() {
+  const { user, users, remove } = useContext(UserContext);
   useTheme("Users", themejson);
+
+  const usersList = users.map(
+    x =>
+      x.id !== user?.id && (
+        <UserTile key={x.id}>
+          <div>
+            <Header>{x.name}</Header>
+            <Subheader>{x.type ? "Common" : "Administrator"}</Subheader>
+          </div>
+          <Delete onClick={() => remove(x.id)} />
+        </UserTile>
+      )
+  );
+
   return (
     <Container>
       <Main>
         <Title>User Management</Title>
+        <Element>
+          {usersList.length === 1 && !usersList[0] ? (
+            <Header>There are no users</Header>
+          ) : (
+            usersList
+          )}
+        </Element>
       </Main>
     </Container>
   );
--- src/views/Private/Users/styles.ts
+++ src/views/Private/Users/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Trash } from "@styled-icons/feather/Trash";
 
 export const Container = styled.main`
   width: 100%;
@@ -19,12 +20,15 @@ export const Container = styled.main`
 `;
 
 export const Main = styled.main`
+  overflow: hidden;
+
   width: 400px;
   height: 500px;
 
   border-radius: 20px;
 
   display: flex;
+  flex-direction: column;
 
   justify-content: center;
   align-items: center;
@@ -34,5 +38,78 @@ export const Main = styled.main`
 
 export const Title = styled.h1`
   font-family: Montserrat;
-  font-family: 2em;
+  font-size: 1.8em;
+
+  color: ${({ theme }) => theme.users?.color};
+`;
+
+export const Users = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  overflow: scroll;
+
+  width: 100%;
+  height: 90%;
+
+  align-items: center;
+`;
+
+export const UserTile = styled.div`
+  cursor: pointer;
+
+  display: flex;
+
+  justify-content: space-between;
+
+  width: 100%;
+  height: 65px;
+
+  background-color: ${({ theme }) => theme.users?.backgroundColor};
+
+  border-bottom: 1px dashed ${({ theme }) => theme.users?.hover};
+
+  &:hover {
+    background-color: ${({ theme }) => `${theme.users?.hover}44`};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const Header = styled.h1`
+  font-family: Montserrat Light;
+  font-size: 1em;
+
+  color: ${({ theme }) => theme.users?.color};
+
+  margin: 10px 0 5px 10px;
+`;
+
+export const Subheader = styled.h1`
+  font-family: Montserrat Light;
+  font-size: 0.8em;
+
+  color: ${({ theme }) => theme.users?.secondary};
+
+  margin: 3px 0 6px 13px;
+`;
+
+export const Delete = styled(Trash)`
+  cursor: pointer;
+
+  height: 25px;
+  width: 25px;
+
+  margin: 20px;
+
+  color: ${({ theme }) => theme.users?.secondary};
+
+  text-shadow: 0 0 10px ${({ theme }) => theme.users?.secondary};
+
+  &:hover {
+    color: ${({ theme }) => theme.users?.color};
+  }
 `;
